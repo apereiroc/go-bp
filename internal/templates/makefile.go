968fc0860ffddd9ddab7a9ebe7fc5cd2a7077e45
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-10-25 18:53:58 +0200

    move user data read to Generate and declare registry

--- internal/templates/makefile.go
+++ internal/templates/makefile.go
@@ -1,10 +1,18 @@
 package templates
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"text/template"
 )
+
+// Init function called by Go runtime
+func init() {
+	RegisterSingleFileTemplate("makefile", func() Template {
+		return MakefileTemplate{ProjectName: "", Author: ""}
+	})
+}
 
 // Hold data specific to a Makefile template
 type MakefileTemplate struct {
@@ -14,9 +22,31 @@ type MakefileTemplate struct {
 
 // Create a Makefile at the specified output path
 func (m MakefileTemplate) Generate(outputPath string) error {
+	// Read user input
+	var author, projectName string
+
+	fmt.Print("Enter author: ")
+	_, err := fmt.Scanln(&author)
+	if err != nil {
+		return err
+	}
+
+	fmt.Print("Enter project name: ")
+	_, err = fmt.Scanln(&projectName)
+	if err != nil {
+		return err
+	}
+
+	m.Author = author
+	m.ProjectName = projectName
+
 	// If outputPath is a directory, join "Makefile" as the default filename
 	fileInfo, err := os.Stat(outputPath)
-	if err == nil && fileInfo.IsDir() {
+	if err != nil {
+		return err
+	}
+
+	if fileInfo.IsDir() {
 		outputPath = filepath.Join(outputPath, "Makefile")
 	}
 
