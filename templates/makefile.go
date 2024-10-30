21d256d6d29b10db50e01186d7476a9949d8223e
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-10-30 09:23:11 +0100

    refactor: switch to embed library for static template files

--- templates/makefile.go
+++ templates/makefile.go
@@ -1,44 +1,47 @@
 package templates
 
 import (
+	"embed"
 	"fmt"
 	"os"
 	"path/filepath"
 	"text/template"
 )
 
-// Init function called by Go runtime
 func init() {
 	RegisterSingleFileTemplate("makefile", func() Template {
 		return MakefileTemplate{ProjectName: "", Author: ""}
 	})
 }
 
-// Hold data specific to a Makefile template
 type MakefileTemplate struct {
 	Author      string
 	ProjectName string
 }
 
-// Create a Makefile at the specified output path
-func (m MakefileTemplate) Generate(outputPath string) error {
-	// Read user input
-	var author, projectName string
-
-	fmt.Print("Enter author: ")
-	_, err := fmt.Scanln(&author)
-	if err != nil {
-		return err
-	}
-
-	fmt.Print("Enter project name: ")
-	_, err = fmt.Scanln(&projectName)
-	if err != nil {
-		return err
-	}
-
+func (m *MakefileTemplate) SetAuthorAndProject(author, projectName string) {
 	m.Author = author
 	m.ProjectName = projectName
+}
+
+//go:embed static/Makefile.tmpl
+var makefileTemplateContent embed.FS
+
+func (m MakefileTemplate) Generate(outputPath string) error {
+	// If author and project name are not set, prompt user for them
+	if m.Author == "" && m.ProjectName == "" {
+		fmt.Print("Enter author: ")
+		_, err := fmt.Scanln(&m.Author)
+		if err != nil {
+			return err
+		}
+
+		fmt.Print("Enter project name: ")
+		_, err = fmt.Scanln(&m.ProjectName)
+		if err != nil {
+			return err
+		}
+	}
 
 	// If outputPath is a directory, join "Makefile" as the default filename
 	fileInfo, err := os.Stat(outputPath)
@@ -50,19 +53,16 @@ func (m MakefileTemplate) Generate(outputPath string) error {
 		outputPath = filepath.Join(outputPath, "Makefile")
 	}
 
-	// Create or overwrite the output file
 	f, err := os.Create(outputPath)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
-	// Parse the template
-	tmpl, err := template.ParseFiles("templates/Makefile.tmpl")
+	tmpl, err := template.ParseFS(makefileTemplateContent, "static/Makefile.tmpl")
 	if err != nil {
 		return err
 	}
 
-	// Apply the template with data
 	return tmpl.Execute(f, m)
 }
